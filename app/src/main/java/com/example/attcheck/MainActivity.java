a08d7e18b8cab21f6b82bfa653b3f953e9de83c3
youchanpark <pakyhc@gmail.com>
2020-01-04 13:54:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/attcheck/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.attcheck;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton btn_facebook_login;
+
+    // **************** Custom Button **************
+    private TextView txtName,txtEmail;
+    //**********************************************
+
+    private CallbackManager callbackManager;
+
+    // ************* 사용자 정보 저장 변수 ************
+    String user_email, user_first_name, user_last_name, user_id;
+    // *******************************************
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_facebook_login = findViewById(R.id.btn_facebook_login);
+        txtName = findViewById(R.id.profile_name);
+        txtEmail = findViewById(R.id.profile_email);
+
+
+        callbackManager = CallbackManager.Factory.create();
+        btn_facebook_login.setReadPermissions(Arrays.asList("email","public_profile"));
+        checkLoginStatus();
+
+        btn_facebook_login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    // ***************************************************
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
+        {
+            if(currentAccessToken==null)
+            {
+                txtEmail.setText("");
+                txtName.setText("");
+
+                Toast.makeText(MainActivity.this,"User Logged out",Toast.LENGTH_LONG).show();
+            }
+            else
+                loadUserProfile(currentAccessToken);
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response)
+            {
+                try {
+                    user_first_name = object.getString("first_name");
+                    user_last_name = object.getString("last_name");
+                    user_email = object.getString("email");
+                    user_id = object.getString("id");
+
+                    txtEmail.setText(user_email);
+                    txtName.setText(user_last_name +user_first_name);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
+
+    private void checkLoginStatus()
+    {
+        if(AccessToken.getCurrentAccessToken()!=null)
+        {
+            loadUserProfile(AccessToken.getCurrentAccessToken());
+        }
+    }
+
+}
